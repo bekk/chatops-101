2e795da575ce3ad11eb975c32e3d4007eeb60fe7
Tine Kleivane <kleivane@gmail.com>
2017-02-05 13:00:59 +0100

    Oppdater boten -> botten

--- scripts/oppgave5.js
+++ scripts/oppgave5.js
@@ -1,13 +1,13 @@
 // Description:
-//   Script for å summere 2 tall sammen
+//   Script for å summere to tall
 //
 // Commands:
-//   hubot summer <tall1> + <tall2> - boten summerer tallene
+//   hubot summer <tall1> + <tall2> - botten summerer tallene
 
 
 module.exports = function (robot) {
     robot.respond(/summer ([0-9]+) \+ ([0-9]+)/, function(res){
-      // res.match[1] -- henter ut første tallet
+      // res.match[1] -- henter ut det første tallet
 
       //code here
 
