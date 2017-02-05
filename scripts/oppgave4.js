23050c6c2226e8792bdc939ec95406f31a14fee5
Tine Kleivane <kleivane@gmail.com>
2017-02-05 12:59:03 +0100

    Legg til et listehint

--- scripts/oppgave4.js
+++ scripts/oppgave4.js
@@ -1,11 +1,12 @@
 // Description:
-//   Script for å vise hvordan de ulike karakterne egentlig ser ut
+//   Script for å vise hvordan de ulike karakterne ser ut
 //
 // Commands:
-//   hubot hvordan så egentlig <navn> ut? - boten gir en link til et bilde
+//   hubot hvordan så egentlig <navn> ut? - botten gir en link til et bilde
 
 
 // Hint 1: for å hente ut navnet se forrige oppgave
 
-// Hint 2: Python dictonary er equivalent i javascript med en object
-// Et object ser typisk slik ut: person = {navn: "bob", alder: 12}
+// Hint 2: En Python dictonary ligner på et Javascript-object
+// Et object ser typisk slik ut: person = {navn: "bob", alder: 12, bilde: "en-link"}
+// En liste av objects vil se slik ut: liste = [{navn: "a"}, {navn: "b"}]
