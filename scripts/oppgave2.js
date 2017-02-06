17b01efeeea5276cd3d89e09e4978e3ce7d57134
Alexander Svendsen <alexandersvendsen1990@gmail.com>
2017-02-06 19:35:26 +0100

    Løs oppgave 2

--- scripts/oppgave2.js
+++ scripts/oppgave2.js
@@ -4,10 +4,25 @@
 // Commands:
 //   hubot hvem har jeg hooket med sist? - botten svarer med en tilfeldig SKAM-karakter
 
-const SKAM_KARAKTERER = ["Navn1", "Navn2"];
+const SKAM_KARAKTERER = [
+  "Vilde",
+  "William",
+  "Noora",
+  "Eva",
+  "Chris",
+  "Kasper",
+  "Eskild",
+  "Sana",
+  "Jonas",
+  "Isak"];
 
 module.exports = function (robot) {
   robot.respond(/hvem har jeg hooket med sist?/, function (res) {
-    //code here
+
+    // Bruk en hjelpemetode på res
+    const randomKarakter = res.random(SKAM_KARAKTERER);
+
+    res.reply(`Hvor full var du da? Du hooket sist med ${randomKarakter}`);
+
   });
 };
