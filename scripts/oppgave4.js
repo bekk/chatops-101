9afc71c75e291452a8dda6d0e72705993ac4d9af
Alexander Svendsen <alexandersvendsen1990@gmail.com>
2017-02-06 20:15:00 +0100

    Løs oppgave 4

--- scripts/oppgave4.js
+++ scripts/oppgave4.js
@@ -5,8 +5,34 @@
 //   hubot hvordan så egentlig <navn> ut - botten gir en link til et bilde
 
 
-// Hint 1: for å hente ut navnet se forrige oppgave
+const SKAM_KARAKTERER = [
+  {navn: "Vilde", bilde: "http://skam.p3.no/wp-content/uploads/2016/09/Vilde_s3.jpg"},
+  {navn: "William", bilde: "http://skam.p3.no/wp-content/uploads/2016/02/william.jpg"},
+  {navn: "Noora", bilde: "http://skam.p3.no/wp-content/uploads/2016/09/noora_ses3.jpg"},
+  {navn: "Eva", bilde: "http://skam.p3.no/wp-content/uploads/2016/09/eva_ses3.jpg"},
+  {navn: "Chris", bilde: "http://skam.p3.no/wp-content/uploads/2016/09/Chris_ses3.jpg"},
+  {navn: "Kasper", bilde: "http://skam.p3.no/wp-content/uploads/2016/09/kasper_s3.jpg"},
+  {navn: "Eskild", bilde: "http://skam.p3.no/wp-content/uploads/2015/09/eskild.jpg"},
+  {navn: "Sana", bilde: "http://skam.p3.no/wp-content/uploads/2016/09/sana_ses3.jpg"},
+  {navn: "Jonas", bilde: "http://skam.p3.no/wp-content/uploads/2016/09/jonas_ses3.jpg"},
+  {navn: "Isak", bilde: "http://skam.p3.no/wp-content/uploads/2016/09/isak_ses3.jpg"}
+];
 
-// Hint 2: En Python dictonary ligner på et Javascript-object
-// Et object ser typisk slik ut: person = {navn: "bob", alder: 12, bilde: "en-link"}
-// En liste av objects vil se slik ut: liste = [{navn: "a"}, {navn: "b"}]
+module.exports = function (robot) {
+  robot.respond(/hvordan så egentlig (\w+) ut/, function (res) {
+
+    const navn = res.match[1];
+
+    const match = SKAM_KARAKTERER.find(function (skamPerson) {
+      return skamPerson.navn.toLowerCase() == navn.toLowerCase();
+    });
+
+    if (match != undefined){
+      res.reply(match.bilde)
+    }
+    else {
+      res.reply(`Fant ikke ${navn}`)
+    }
+
+  });
+};
