d8fde94a880533caebf6b1d9cf7fe27b08ef48fd
Alexander Svendsen <alexandersvendsen1990@gmail.com>
2017-02-06 20:17:18 +0100

    Løs oppgave 5

--- scripts/oppgave5.js
+++ scripts/oppgave5.js
@@ -7,10 +7,11 @@
 
 module.exports = function (robot) {
   robot.respond(/summer ([0-9]+) \+ ([0-9]+)/, function (res) {
-    // res.match[1] -- henter ut det første tallet
 
-    //code here
+    const tall1 = parseInt(res.match[1]);
+    const tall2 = parseInt(res.match[2]);
 
+    res.reply(`Summen av ${tall1} + ${tall2} = ${tall1 + tall2}`);
 
   });
 };
