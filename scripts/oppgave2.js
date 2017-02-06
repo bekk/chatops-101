16506c3d242825d51f19cd82c24d67e29ffb6662
Alexander Svendsen <alexandersvendsen1990@gmail.com>
2017-02-06 19:35:50 +0100

    Fix teksten litt i oppgave 2

--- scripts/oppgave2.js
+++ scripts/oppgave2.js
@@ -2,12 +2,12 @@
 //   Velger en tilfeldig SKAM-karakter i listen når du spør hvem har jeg hooket med sist
 //
 // Commands:
-//   hubot hvem har jeg hooket med sist? - botten svarer med en tilfeldig SKAM-karakter
+//   hubot hvem har jeg hooket med sist - botten svarer med en tilfeldig SKAM-karakter
 
 const SKAM_KARAKTERER = ["Navn1", "Navn2"];
 
 module.exports = function (robot) {
-  robot.respond(/hvem har jeg hooket med sist?/, function (res) {
+  robot.respond(/hvem har jeg hooket med sist/, function (res) {
     //code here
   });
 };
