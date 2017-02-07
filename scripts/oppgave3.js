ce21deb1c75657f99ccd4eb5168666fa6e759325
Guro <guro.seternes@bekk.no>
2017-02-07 17:27:47 +0100

    Rettet opp tekster slik at opg 1 og 2 ikke går i bena på hverandre

--- scripts/oppgave3.js
+++ scripts/oppgave3.js
@@ -3,9 +3,9 @@
 //   når du spør hvem har jeg hooket med sist
 //
 // Commands:
-//   hubot hvem har jeg hooket med sist av guttene - botten svarer et tilfeldig guttenavn
-//   hubot hvem har jeg hooket med sist av jentene - botten svarer et tilfeldig jentenavn
-//   hubot hvem har jeg hooket med sist av .... - botten svarer en tilfeldig person
+//   hubot hvem av guttene har jeg hooket med sist? - botten svarer et tilfeldig guttenavn
+//   hubot hvem av jentene har jeg hooket med sist? - botten svarer et tilfeldig jentenavn
+//   hubot hvem av ...  har jeg hooket med sist?- botten svarer en tilfeldig person
 
 // Hint 1: dersom du lurer på hva som er lagret i en variabel kan du skrive den ut med
 // console.log("tekst", variabel)
@@ -14,7 +14,7 @@ const SKAM_KARAKTERER_GUTTER = ["gutt1", "gutt2"];
 const SKAM_KARAKTERER_JENTER = ["jente1", "jente2"];
 
 module.exports = function (robot) {
-  robot.respond(/hvem har jeg hooket med sist av (\w+)/, function (res) {
+  robot.respond(/hvem av (\w+) har jeg hooket med sist /, function (res) {
     // res.match[1] -- henter ut ordet på plassen til (\w+) i det du skrev til botten
 
     // code here
