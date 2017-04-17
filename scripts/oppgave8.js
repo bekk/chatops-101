6742815df40cf7eca389052e0b0ac85d6f950892
Guro <guro.seternes@bekk.no>
2017-04-17 15:59:29 +0200

    Lagt til lf for yr-oppgaven

---
+++ scripts/oppgave8.js
@@ -0,0 +1,26 @@
+// Description:
+//   Script for å finne ut hva man bør ha på seg i dag
+//
+// Commands:
+//   hubot hva bør jeg ha på meg når jeg går ut
+
+const request = require('request');
+const xml2json = require('xml2json');
+const bergenYrURL = 'http://www.yr.no/sted/Norge/Hordaland/Bergen/Bergen/varsel_time_for_time.xml';
+
+module.exports = function (robot) {
+  robot.respond(/hva bør jeg ha på meg når jeg går ut/, function (res) {
+    request(bergenYrURL, function (error, response) {
+      const jsonRep = xml2json.toJson(response.body);
+      const yrData = JSON.parse(jsonRep);
+      const regnMengde = yrData.weatherdata.forecast.tabular.time[0].precipitation.value;
+      if (regnMengde < 1) {
+        res.reply("Endelig pent vær, legg paraplyen hjemme");
+      } else if (regnMengde >= 1 && regnMengde < 3) {
+        res.reply("Business as usual, ta med paraply");
+      } else {
+        res.reply("Himmelen har åpnet seg, ta frem allværsjakka og støvler")
+      }
+    });
+  });
+};
