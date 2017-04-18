53dd794e29d1b7defe593028096d65fc9584298d
Kristine Steine <kristine.steine@bekk.no>
2017-04-18 22:51:39 +0200

    Bruk request-bibliotek

--- scripts/oppgave9.js
+++ scripts/oppgave9.js
@@ -5,36 +5,37 @@
 //   hubot hvem leder eliteserien
 //   hubot hvordan går det med <lag>
 
+const request = require('request');
 const url = "https://api.vglive.no/v1/vg/tournaments/seasons/1439/standings/live";
 
 module.exports = function (robot) {
   robot.respond(/hvem leder eliteserien/i, function (msg) {
-    robot.http(url).get()(function (err, res, body) {
-      if (err) {
-        msg.send("Noe gikk galt!", err.message);
+    request(url, function (error, response, body) {
+      if (error) {
+        msg.send("Noe gikk galt!", error.message);
         return;
       }
-      const response = JSON.parse(body);
-      const leaderTeamId = response.standings[0].results[0].participantId;
-      const leaderTeam = response.participants.find(team => team.id === leaderTeamId);
+      const data = JSON.parse(body);
+      const leaderTeamId = data.standings[0].results[0].participantId;
+      const leaderTeam = data.participants.find(team => team.id === leaderTeamId);
       msg.send(`${leaderTeam.name} leder eliteserien! :soccer:`);
     });
   });
 
   robot.respond(/hvordan går det med (\w+)\??/i, function (msg) {
     const teamName = msg.match[1];
-    robot.http(url).get()(function (err, res, body) {
-      if (err) {
-        msg.send("Noe gikk galt!", err.message);
+    request(url, function (error, response, body) {
+      if (error) {
+        msg.send("Noe gikk galt!", error.message);
         return;
       }
-      const response = JSON.parse(body);
-      const participant = response.participants.find(team => team.name.toLowerCase().match(teamName.toLowerCase()));
+      const data = JSON.parse(body);
+      const participant = data.participants.find(team => team.name.toLowerCase().match(teamName.toLowerCase()));
       if (!participant) {
         msg.send(`Fant ikke noen lag med det navnet, prøv igjen`);
         return;
       }
-      const standing = response.standings[0].results.find(standing => standing.participantId === participant.id);
+      const standing = data.standings[0].results.find(standing => standing.participantId === participant.id);
       msg.send(`${participant.name} er på ${standing.rank}. plass i eliteserien med ${standing.points} poeng :soccer:`);
     })
   })
