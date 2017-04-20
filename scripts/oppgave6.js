e1a043dcf78a872a3126b47c5b8fb7de4454a401
Guro <guro.seternes@bekk.no>
2017-04-20 11:58:42 +0200

    Oppdatert oppgave 6 slik at den teller ned antall dager til ferien

--- scripts/oppgave6.js
+++ scripts/oppgave6.js
@@ -1,20 +1,20 @@
 // Description:
-//   Script for å telle antall dager siden SKAM sesong 4 startet 
+//   Script for å telle antall dager til sommerferien 
 //
 // Commands:
-//   hubot hvor mange dager er det siden skam startet - boten svarer med antall dager
+//   hubot hvor mange dager er det igjen til sommerferien - boten svarer med antall dager
 
 // Hint 1: bruk moment-biblioteket her https://momentjs.com/. PS: Vi har importert det for deg på neste linje
 
 const moment = require('moment');
 
 module.exports = function (robot) {
-  robot.respond(/hvor mange dager er det siden skam startet/, function (res) {
+  robot.respond(/hvor mange dager er det igjen til sommerferien/, function (res) {
     const now = moment();
-    const seasonStart = moment("2017-04-07");
+    const lastExamDate = moment("2017-06-01");
 
-    const diff = now.diff(seasonStart, "days");
+    const diff = lastExamDate.diff(now, "days");
 
-    res.reply(`SKAM startet for ${diff} dager siden`);
+    res.reply(`Det er bare ${diff} dager til sommerferien!`);
   });
 };
