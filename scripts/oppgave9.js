c562a3baa1b3c900ce492d0b2bd355f15df0c585
Kristine Steine <kristine.steine@bekk.no>
2019-02-25 16:19:55 +0100

    Oppdater LF for fotball

--- scripts/oppgave9.js
+++ scripts/oppgave9.js
@@ -1,24 +1,24 @@
 // Description:
-//   Script for å sjekke resultater fra Eliteserien 2017
+//   Script for å sjekke resultater fra Premier League 2018/2019
 //
 // Commands:
-//   hubot hvem leder eliteserien
+//   hubot hvem leder Premier League
 //   hubot hvordan går det med <lag>
 
 const request = require('request');
-const url = "https://api.vglive.no/v1/vg/tournaments/seasons/1439/standings/live";
+const url = "https://vglive.no/api/vg/tournaments/seasons/1776/standings?type=live-changes";
 
 module.exports = function (robot) {
-  robot.respond(/hvem leder eliteserien/i, function (msg) {
+  robot.respond(/hvem leder premier league/i, function (msg) {
     request(url, function (error, response, body) {
       if (error) {
         msg.send("Noe gikk galt!", error.message);
         return;
       }
       const data = JSON.parse(body);
-      const leaderTeamId = data.standings[0].results[0].participantId;
-      const leaderTeam = data.participants.find(team => team.id === leaderTeamId);
-      msg.send(`${leaderTeam.name} leder eliteserien! :soccer:`);
+      const leaderTeamId = data.standings[0].teamStandings[0].teamId;
+      const leaderTeam = Object.values(data.participants).find(team => team.id === leaderTeamId);
+      msg.send(`${leaderTeam.name} leder Premier League! :soccer:`);
     });
   });
 
@@ -30,13 +30,13 @@ module.exports = function (robot) {
         return;
       }
       const data = JSON.parse(body);
-      const participant = data.participants.find(team => team.name.toLowerCase().match(teamName.toLowerCase()));
+      const participant = Object.values(data.participants).find(team =>team.name.toLowerCase().match(teamName.toLowerCase()));
       if (!participant) {
         msg.send(`Fant ikke noen lag med det navnet, prøv igjen`);
         return;
       }
-      const standing = data.standings[0].results.find(standing => standing.participantId === participant.id);
-      msg.send(`${participant.name} er på ${standing.rank}. plass i eliteserien med ${standing.points} poeng :soccer:`);
+      const standing = data.standings[0].teamStandings.find(standing => standing.teamId === participant.id);
+      msg.send(`${participant.name} er på ${standing.rank}. plass i Premier League med ${standing.points} poeng :soccer:`);
     })
   })
 }
